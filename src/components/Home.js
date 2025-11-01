239ede63bea518197320acf2c9043275b095da61
Laura Fernandes <fernandes.laura.2023@gmail.com>
2025-11-01 15:08:59 -0700

    adding pages

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>I'm Laura</h1>
+            <h2>Welcome to My Website</h2>
+            <p>
+                I'm a Psychology and Computer Science combined major international student! I'm from Brazil but I have been
+                living here in Victoria with my sisters for four years now! It has been a great experience for us.
+            </p>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/Projects.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Projects = () => {
+    const projects = [
+     {
+        name: "Personal Portfolio Website",
+        description: "A website to test different layouts, to test and showcase my skills, projects, and experiences.",
+        tech: "React, HTML, CSS, React Router",
+        link: "https://github.com/FernandesLaura0/Projects"
+        
+     },
+     {
+        name: "Technical project-Python and Java"
+     }
+    ];
+    return (
+        <div>
+            <div>
+                {projects.map((project, index) => (
+                    <div key={index}>
+                        <h2><strong>{project.name}</strong></h2>
+                        <p>{project.description}</p>
+                        <p><em>{project.tech}</em></p>
+                        {project.link && (
+                            <a
+                            href={project.link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            >
+                                View on GitHub
+                            </a>
+                        )}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
