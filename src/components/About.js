43912cec677567c49d75a47f1391c5d04917d5bf
Laura Fernandes <fernandes.laura.2023@gmail.com>
2025-12-09 19:49:16 -0800

    syncchanged the about page again

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <div className="content">
+            <h1>More about me:</h1>
+            <h2>Studying at University of Victoria</h2>
+            <p>
+                Fourth year student 
+            </p>
+            </div>
+        </div>
+    );
+};
+
+export default About;
