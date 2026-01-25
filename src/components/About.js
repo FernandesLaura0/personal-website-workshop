28cf76038265a2b0f326c2ea80132a5c67faf716
Laura Fernandes <fernandes.laura.2023@gmail.com>
2026-01-24 21:03:33 -0800

    creating the animation to flip the pages

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,12 @@ import "./About.css";
 const About = () => {
   return (
     <div className="about-container">
+        <input type="checkbox" id="checkbox-cover"></input>
       <div className="book">
-        <div className="cover"></div>
+        <div className="cover">
+            <label for="checkbox-cover"></label>
+        </div>
+        <div className="page"></div>
         <div className="back-cover"></div>
       </div>
     </div>
