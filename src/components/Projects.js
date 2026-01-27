60700695ae414d09b7dba7dd9218248c44ecaeec
Laura Fernandes <fernandes.laura.2023@gmail.com>
2026-01-27 12:35:39 -0800

    Added the animation to the page, for now the images show up when I click in the box

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Projects.css";
+import figma1 from "../components/Screenshots/figma1.png";
+import figma2 from "../components/Screenshots/figma2.png";
+import figma3 from "../components/Screenshots/figma3.png";
+import figma4 from "../components/Screenshots/figma4.png";
+import figma5 from "../components/Screenshots/figma5.png";
+import figma6 from "../components/Screenshots/figma6.png";
 
 const Projects = () => {
+
+    const [activeIndex, setActiveIndex] = useState(null);
+
     const projects = [
      {
         name: "Personal Portfolio Website",
@@ -18,17 +27,19 @@ const Projects = () => {
      },
      {
         name: "Figma projects",
-        description: "Link to my projects on Figma envolving software desing",
+        description: "Link to my projects on Figma envolving software design",
         link: "https://www.figma.com/files/team/1350981228662349295/all-projects?fuid=1350981224470866735",
         linkLabel: "View on Figma",
-        images: [/components/screnshot/figma1.png]
+        images: [figma1, figma2, figma3, figma4, figma5, figma6 ]
      }
     ];
+
     return (
         <div className="projects-container">
             <div className="projects-content">
                 {projects.map((project, index) => (
-                    <div key={index} className="projects-card">
+                    <div key={index} className="projects-card" onClick={() => setActiveIndex(activeIndex == index ? null:index)}>
+                        
                         <h2><strong>{project.name}</strong></h2>
                         <p>{project.description}</p>
                         <p className="tech"><em>{project.tech}</em></p>
@@ -38,10 +49,24 @@ const Projects = () => {
                             target="_blank"
                             rel="noopener noreferrer"
                             className="github-link"
+                            onClick={(e) => e.stopPropagation()} // prevent card toggle when clicking on the link
                             >
                                 {project.link.includes("figma.com") ? "View on Figma" : "View on GitHub"}
                             </a>
                         )}
+
+                        {project.images && activeIndex == index && (
+                            <div className="project-images">
+                                {project.images.map((img, i) => (
+                                    <img
+                                    key = {i}
+                                    src = {img}
+                                    alt = {`${project.name} screenshot ${i + 1}`}
+                                    />
+                                ))}
+                            </div>
+                        )}
+
                     </div>
                 ))}
             </div>
