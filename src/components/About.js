6d06ea376eb1635be31b7b0c6c6bc7cec966a1cb
Laura Fernandes <fernandes.laura.2023@gmail.com>
2026-02-18 20:54:37 -0800

    fixing small mistakes

--- src/components/About.js
+++ src/components/About.js
@@ -10,46 +10,46 @@ const About = () => {
         <input type="checkbox" id="checkbox-page3"></input>
       <div className="book">
         <div className="cover">
-            <label for="checkbox-cover"></label>
+            <label htmlFor="checkbox-cover"></label>
         </div>
-        <div class="page" id="page1">
+        <div className="page" id="page1">
           <div class="front-page">
             <p>
               More about me:
             </p>
-            <label class="next" for="checkbox-page1">
-              <i class="fas-fa-chevron-right">
+            <label className="next" for="checkbox-page1">
+              <i className="fas-fa-chevron-right">
               </i>
             </label>
           </div>
-          <div class="back-page">
+          <div className="back-page">
             <img src="1.jpg"></img>
-            <label class="prev" for="checkbox-page1">
-              <i class="fas-fa-chevron-right">
+            <label className="prev" htmlFor="checkbox-page1">
+              <i className="fas-fa-chevron-right">
               </i>
               </label>
           </div>
         </div>
 
-        <div class="page" id="page2">
-          <div class="front-page">
+        <div className="page" id="page2">
+          <div className="front-page">
             <h2> Page 2</h2>
             <p> I am doing Psychology and Computer Science Computer
               combined major because I always believed that people and computers 
               are very much connected so getting to learn about both has been really
               interesting.</p>
-              <label class="next" for="checkbox-page2"><i
-              class="fas fa-chevron-right"></i></label>
+              <label className="next" htmlFor="checkbox-page2"><i
+              className="fas fa-chevron-right"></i></label>
+          
+            <div className="back-page">
+              <img src="2.jpg"></img>
+              <label className="prev" htmlFor="checkbox-page2"><i 
+              className="fas fa-chevron-left"></i></label>
+            </div>
           </div>
         </div>
-
-        <div class="back-page">
-          <img src="2.jpg"></img>
-          <label class="prev" for="checkbox-page2"><i 
-          class="fas fa-chevron-left"></i></label>
-        </div>
-        <div class="page" id="page3">
-          <div class="front-page">
+        <div className="page" id="page3">
+          <div className="front-page">
             <h2> Page 3</h2>
             <p>More info about me:</p>
           </div>
